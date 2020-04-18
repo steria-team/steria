8122deac085e01a7d0b5e36fcc18ac3d09aa7232
pen-dev <pen@stacksoft.ru>
2020-04-19 01:23:04 +0300

    Навык умеет спрашивать название улицы и номер дома

--- src/api.py
+++ src/api.py
@@ -13,11 +13,12 @@ import os
 import pathlib
 
 
-import flask
 import lxml
 from flask import Flask, request
 from flask_cors import CORS
 from lxml import html
+
+from . import house
 
 app = Flask(__name__)
 CORS(app)
@@ -25,7 +26,7 @@ CORS(app)
 DIR_SCRIPT: pathlib.Path = pathlib.Path(os.path.dirname(os.path.realpath(__file__)))
 
 # Хранилище данных о сессиях.
-sessionStorage = {}
+sessionStorage: typing.Dict[str,  house.House] = {}
 
 @app.after_request
 def after_request(response):
@@ -62,15 +63,15 @@ def main():
 
 
 # Функция для непосредственной обработки диалога.
-def parse_html(param):
-    example_page_path: str = os.path.join(DIR_SCRIPT.parent, 'example.html')
+def parse_html(adress: str):
+    example_page_path: str = os.path.join(DIR_SCRIPT.parent, 'page_example', 'example.html')
     with open(example_page_path, 'r', encoding='ISO-8859-1') as f:
         text = f.read()
     tree: lxml.html.HtmlElement = html.fromstring(html=text)
     # todo: доделать
+    # .//*[@class='address' and text()=', 2']/a[contains(text(), 'Очаковская ул.')]/preceding-sibling::*[@class='cssHouseHead']
     tree.xpath("//div[@class='address' ]/a[contains(text(),'Суво')]")
     return
-
 
 
 def handle_dialog(req, res):
@@ -80,23 +81,27 @@ def handle_dialog(req, res):
     if sessionStorage.get(user_id) is None:
         # Это новый пользователь.
         # Инициализируем сессию и поприветствуем его.
-
-        res['response']['text'] = 'Какая улица вас интересует?'
-        res['response']['buttons'] = get_suggests(user_id)
+        sessionStorage[user_id]: typing.Dict[house.House] = house.House()
+        res['response']['text'] = sessionStorage[user_id].ask()
         return
+    else:
+        user_data: house.House = sessionStorage[user_id]
+        user_data.set_answer(text=req['request']['original_utterance'].lower())
+        res['response']['text'] = user_data.ask()
 
-    # Обрабатываем ответ пользователя.
-    support_street: typing.List[str] = ['Кавалергардская ул.']
-    if req['request']['original_utterance'].lower() in support_street:
-        # Пользователь согласился, прощаемся.
-        res['response']['text'] = parse_html(req['request']['original_utterance'])
-        return
-
-    # Если нет, то убеждаем его купить духи!
-    res['response']['text'] = 'Все говорят "%s", а ты купи духи!' % (
-        req['request']['original_utterance']
-    )
-    res['response']['buttons'] = get_suggests()
+    return
+    # # Обрабатываем ответ пользователя.
+    # support_street: typing.List[str] = ['Кавалергардская ул.']
+    # if req['request']['original_utterance'].lower() in support_street:
+    #     # Пользователь согласился, прощаемся.
+    #     res['response']['text'] = parse_html(req['request']['original_utterance'])
+    #     return
+    #
+    # # Если нет, то убеждаем его купить духи!
+    # res['response']['text'] = 'Все говорят "%s", а ты купи духи!' % (
+    #     req['request']['original_utterance']
+    # )
+    # res['response']['buttons'] = get_suggests()
 
 
 # Функция возвращает две подсказки для ответа.
---
+++ src/house.py
@@ -0,0 +1,24 @@
+class House:
+    def __init__(self):
+        self.street = None
+        self.address = None
+
+    def ask(self):
+        if self.street is None:
+            return self._ask_street()
+        elif self.address is None:
+            return self._ask_address()
+        else:
+            return f'Ваша улица {self.street}, а дом {self.address}'
+
+    def _ask_address(self):
+        return 'Назовите номер дома? Например, 7'
+
+    def _ask_street(self):
+        return 'Какая улица вас интересует? Например, Кавалергардская'
+
+    def set_answer(self, text: str):
+        if self.street is None:
+            self.street = text
+        elif self.address is None:
+            self.address = text
