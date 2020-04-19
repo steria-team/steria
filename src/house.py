10a6f407593cfad27f3326e31a207c60b98aeee1
pen-dev <pen@stacksoft.ru>
2020-04-19 14:11:16 +0300

    renamed

--- src/house.py
+++ src/house.py
@@ -2,15 +2,15 @@ class House:
     def __init__(self):
         self.street = None
         # todo: rename number
-        self.address = None
+        self.number = None
 
     def ask(self):
         if self.street is None:
             return self._ask_street()
-        elif self.address is None:
+        elif self.number is None:
             return self._ask_address()
         else:
-            return f'Ваша улица {self.street}, а дом {self.address}'
+            return f'Ваша улица {self.street}, а дом {self.number}'
 
     def _ask_address(self):
         return 'Назовите номер дома? Например, 7'
@@ -21,5 +21,5 @@ class House:
     def set_answer(self, text: str):
         if self.street is None:
             self.street = text
-        elif self.address is None:
-            self.address = text
+        elif self.number is None:
+            self.number = text
