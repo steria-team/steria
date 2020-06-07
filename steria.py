3aeb27ecc799d28d9a06700153ddf92114aa74dd
JPapov <eg.wow@bk.ru>
2020-06-08 01:15:38 +0300

    added a comment to resource API

--- steria.py
+++ steria.py
@@ -4,8 +4,10 @@ from flask_restful import Api
 from steriaserver import app
 from steriaserver.HouseManager import Quote
 
+# добавил resource к API и задал путь
 api = Api(app)
 api.add_resource(Quote, "/ai-quotes", "/ai-quotes/", "/ai-quotes/<int:r_adress>")
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
