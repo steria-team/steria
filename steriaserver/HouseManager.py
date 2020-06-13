73db005bf0d10390fccae296971ff917e786a438
pen <pen@stacksoft.ru>
2020-06-13 20:04:20 +0300

    удалил комментарии

--- steriaserver/HouseManager.py
+++ steriaserver/HouseManager.py
@@ -5,18 +5,6 @@ import requests
 import json
 from urllib import parse
 
-# Вызов API carto по адресу
-# apiUrl = f'https://nslavin.carto.com/api/v2/sql?q=select%20*%20from%20public.saint_pv4_050620%20where%20r_adress%20like{r_adress}'
-# reqAddress = requests.get(apiUrl)
-# print("Status code:", reqAddress.status_code)
-# responseJson = reqAddress.json()
-
-
-
-# Сырой пример api сервиса для фронта.
-
-
-
 
 class Quote(Resource):
     def get(self, r_adress):
@@ -37,24 +25,3 @@ class Quote(Resource):
         print(reqAddress.text)
         return reqAddress.json()
 
-
-
-
-
-
-
-    # def post(self, r_adress):
-    #     parser = reqparse.RequestParser()
-    #     parser.add_argument("author")
-    #     parser.add_argument("quote")
-    #     params = parser.parse_args()
-    #     for quote in ai_quotes:
-    #         if(id == quote["r_adress"]):
-    #             return f"Quote with r_adress {r_adress} already exists", 400
-    #     quote = {
-    #         "r_adress": str(r_adress),
-    #         "author": params["author"],
-    #         "quote": params["quote"]
-    #     }
-    #     ai_quotes.append(quote)
-    #     return quote, 201
