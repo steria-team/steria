20ecffa380c4249b985e7e0051937dc7440f36f9
pen <pen@stacksoft.ru>
2020-06-21 19:11:31 +0300

    парсинг аргумента  address в url (url?address=value)

--- steriaserver/api.py
+++ steriaserver/api.py
@@ -1,13 +1,25 @@
+from typing import Dict
 from flask_restful import Resource
+from flask_restful import reqparse
 from steriaserver.houselib import HouseData
+# from steriaserver import api
+
+parser = reqparse.RequestParser()
+parser.add_argument(
+    'address',
+    dest='address',
+    type=str,
+    required=True
+)
 
 
+# @api.resource('/v1/house')
 class HouseResource(Resource):
 
-    def get(self, address: str):
-
+    def get(self):
+        args: Dict[str, str] = parser.parse_args()
         try:
-            data: HouseData = HouseData.from_carto(address)
+            data: HouseData = HouseData.from_carto(args['address'])
         except Exception:
             return 'Error', 500
         return {
