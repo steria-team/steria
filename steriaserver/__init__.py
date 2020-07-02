082a0ba6ab1a77c0aafda06ffc946b2fc9566401
pen <pen@stacksoft.ru>
2020-07-02 13:04:45 +0300

    интеграция с sentry для мониторинга ошибок на сервере

--- steriaserver/__init__.py
+++ steriaserver/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+steria_dsn: str = \
+    'https://6d5c1c456da44203afe605afda1a4656@o414928.ingest.sentry.io/5305244'
+
+sentry_sdk.init(
+    dsn=steria_dsn,
+    integrations=[FlaskIntegration()]
+)
 
 app = Flask(__name__)
 api = Api(app)
