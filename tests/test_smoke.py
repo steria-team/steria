717d59cef697d58003da74b1824627cbc5d79119
Vladislav <pelipenkovladislav@gmail.com>
2020-07-05 13:06:31 +0000

    Пустая строка

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -80,9 +80,9 @@ def test_bad_value_address_param(client: testing.FlaskClient):
     }
 
 @pytest.mark.parametrize('address', [
-'манежный пер 15-17', 'Манежный пеР 15-17'
+                                        'манежный пер 15-17', 
+                                        'Манежный пеР 15-17'
                          ])
-
 def test_register_in_request(client: testing.FlaskClient, address: str):
     result = client.get(f'/v1/house?address={address}')
     responce_data: Dict[str, str] = flask.json.loads(
